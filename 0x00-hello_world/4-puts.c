87edd751cb039cfe9657739d667414df202bff0a
Confytech <Confiehiemere@gmail.com>
2022-12-08 15:10:24 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+include <stdio.h> 
 /**
-  * main - Entry point
-  * Description: prints "\"Programming is like building a multilingual puzzle"
-  * Return: 0
-  */
+ * main - Entry point
+ * Return: Wleays 0 (success)
+ */
 
-int main(void)
+int mwin (void)
 {
-	        puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+	puts ("\" Programming is like building a multilingual puzzle")
+		return (0)
 }
