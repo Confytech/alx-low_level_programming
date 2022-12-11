c9b0bd8f72b40365517a8e4bacb6f543035a2053
Confytech <Confiehiemere@gmail.com>
2022-12-11 09:28:58 -0800

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: prints allsingle digit numbers of base 10 from 0
+ * Description: prints all single digit numbers of base 10 from 0
  * Return: Always 0 (success)
  */
 int main(void)
