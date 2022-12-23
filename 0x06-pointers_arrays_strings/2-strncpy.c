cd1a54b9c64d2356a7a4ab88425d3f90dcfbea51
Confytech <Confiehiemere@gmail.com>
2022-12-22 20:24:33 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[j] = src[j];
-		j++;
+		dest[j] = src[j]; j++;
 	}
 	while (j < n)
 	{
-		dest[j] = '\0';
-		j++;
+		dest[j] = '\0'; j++;
 	}
 
 	return (dest);
