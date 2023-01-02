04b47a285cbceb4cd2d8330d79c378acefea7c14
Confytech <ehiemereconfidence23@gmail.com>
2023-01-02 11:30:30 -0800

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int b, c;
 
-	for (i = 0; i < 8; i++)
+	for (b = 0; b < 8; b++)
 	{
-		for (j = 0; j < 8; j++)
+		for (c = 0; c < 8; c++)
 		{
-	_putchar(a[i][j]);
+			_putchar(a[b][c]);
 		}
 		_putchar('\n');
 	}
