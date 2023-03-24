f0029c9dbab5529b801ee9b0a48df0b737aa4e76
Confytech <confiehiemere@gmail.com>
2023-03-24 12:34:28 -0700

    my task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _isupper(int c);
@@ -9,6 +9,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
